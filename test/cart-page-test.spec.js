c01fa210ea970a5b7f580a2346bf37354297c07e
Vlad <vyuzkevych@gmail.com>
2024-03-30 14:33:46 +0200

    add tests and page objects for cart page

---
+++ test/cart-page-test.spec.js
@@ -0,0 +1,35 @@
+//@ts-check
+
+import { test } from "@playwright/test";
+
+test.describe("test cart page", () => {
+
+    test.beforeEach( async ({ page }) => {
+
+    })
+
+    test("verify that added book is present in cart", async () => {
+
+    })
+
+    test("verify that by click on '+' button quantity is increased by one", async () => {
+
+    })
+
+    test("verify that by click on '-' button quantity is decreased by one", async () => {
+
+    })
+
+    test("verify that by changing quantity the total price is increased", async () => {
+
+    })
+
+    test("verify that by clicking on 'bin' button the book is removed", async () => {
+
+    })
+
+    test("verify that if cart contains several books the total cart price updated properly", async () => {
+        
+    })
+
+})
---
+++ test/page-objects/CartPage.js
@@ -0,0 +1,23 @@
+//@ts-check
+
+import { expect } from "@playwright/test";
+
+export class CartPage {
+
+    /**
+   * @param {import('@playwright/test').Page} page
+   */
+
+    constructor(page) {
+        this.page = page;
+        this.cartBody = page.locator(".mdc-data-table__content");
+        this.bookTitle = page.locator(".mdc-data-table__content :nth-child(2) > a");
+        this.decreaseQuantityBtn = page.locator("div[class='d-flex align-items-center'] > div:nth-child(1)");
+        this.increaseQuantityBtn = page.locator("div[class='d-flex align-items-center'] > div:nth-child(3)");
+        this.quantity = page.locator("div[class='d-flex align-items-center'] > div:nth-child(2)")
+        this.totalPrice = page.locator("tbody[role='rowgroup'] > tr > td:nth-child(5)");
+        this.binBtn = page.locator("button[mattooltip='Delete item']");
+        this.checkoutBtn = page.getByText(" CheckOut ");
+    }
+
+}
