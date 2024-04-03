a909b5bf3e55ae7b47d72d702d5690649bc827c2
Vlad <vyuzkevych@gmail.com>
2024-04-03 11:19:16 +0300

    added new page object for orders page

---
+++ test/page-objects/OrdersPage.js
@@ -0,0 +1,14 @@
+//@ts-check
+
+export class OrdersPage {
+
+    /**
+   * @param {import('@playwright/test').Page} page
+   */
+
+    constructor(page) {
+        this.page = page;
+        this.orderId = page.locator("td[role='cell']").first();
+        this.orderDate = page.locator("tbody[class='mdc-data-table__content'] > tr > td:nth-child(2)");
+    }
+}
