9e525e70614a81d40a60602d5ce5201175749a60
Vlad <vyuzkevych@gmail.com>
2024-04-23 11:37:42 +0300

    add POST test example

--- api-examples/tests/api-test.spec.ts
+++ api-examples/tests/api-test.spec.ts
@@ -4,7 +4,7 @@ test.describe("api test examples", () => {
 
     test("GET request example", async ({ request }) => {
         const resp = await request.get("/api/users?page=1");
-        // verify status code
+        // verify response status code
         expect(resp.status()).toBe(200);
         expect(resp.statusText()).toBe("OK");
         // verify amount of records per page
@@ -16,4 +16,23 @@ test.describe("api test examples", () => {
             expect(typeof body.data[i].id).toBe("number");
         }
     })
+
+    test("POST request example", async ({ request }) => {
+        const resp = await request.post("/api/users", {
+            data: {
+                "name": "test",
+                "job": "test"
+            }
+        });
+        // verify response status code
+        expect(resp.status()).toBe(201);
+        // verify response data
+        const body = await resp.json();
+        expect(body.name).toBe("test");
+        expect(body.job).toBe("test");
+        expect(body.id).not.toHaveLength(0);
+        let date = new Date().toISOString().slice(0, 10);
+        expect(body.createdAt).toContain(date)
+    })
+
 })
